5ce3cc16df7ce4c341bf221e1427d04708c53004
calvin <c.c.e.lee@student.tue.nl>
2022-03-31 14:43:59 +0200

    fixed linear regression

--- functions/linear_regression.py
+++ functions/linear_regression.py
@@ -4,6 +4,7 @@ from sklearn.model_selection import TimeSeriesSplit
 from sklearn import linear_model
 import statsmodels.api as sm
 
+
 def one_hot_encode(df, column, prefix):
     """pandas one hot encoder
 
@@ -38,16 +39,25 @@ def time_diff(df, outlier):
     within a trace and converts the value to log"""
     df['time_diff'] = (
         df['nextTime'] - df['event time:timestamp']).dt.total_seconds()
+
+    df['next_time'] = df['event time:timestamp'].shift(1)
+    df['next_time'] = (df['event time:timestamp'] -
+                       df['next_time']).dt.total_seconds()
     df = df.dropna().reset_index(drop=True)
+
+    cols = df.columns.tolist()
+    cols.remove('time_diff')
+    cols = cols + ['time_diff']
+    df = df[cols]
 
     df = df[df['time_diff'] >= 0]
     df['time_diff'] = np.log(df['time_diff'].replace(0, np.nan))
     df['time_diff'] = df['time_diff'].replace(np.nan, 0)
-    
+
     if outlier == 'keep':
         pass
     elif outlier == 'remove':
-        df = df[df['time_diff']<604800]
+        df = df[df['time_diff'] < 604800]
 
     return df.drop(['event time:timestamp', 'nextTime'], axis=1)
 
@@ -66,6 +76,7 @@ def cross_validate(X, Y):
     output = []
     ts = TimeSeriesSplit(gap=175, max_train_size=None,
                          n_splits=5, test_size=None)
+
     folds = list(ts.split(X))
     for train_index, test_index in folds:
         X_train, X_test = X[train_index], X[test_index]
@@ -80,6 +91,7 @@ def cross_validate(X, Y):
 
     return output, model
 
+
 def del_intersection(train, test):
     lst_tr = train['case concept:name'].unique().tolist()
     lst_te = test['case concept:name'].unique().tolist()
@@ -89,6 +101,3 @@ def del_intersection(train, test):
     train = train[~train['case concept:name'].isin(lst_int)]
     test = test[~test['case concept:name'].isin(lst_int)]
     return train, test
-
-
-
